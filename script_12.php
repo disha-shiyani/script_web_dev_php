b59d1e4506885f5759105a1b1095a02fe4aaef7f
disha-shiyani <143687230+disha-shiyani@users.noreply.github.com>
2023-09-18 13:42:12 +0530

    Develop a PHP program that uses echo to display an unordered list (<ul>) with three list items (<li>)  containing different colors.

---
+++ script_12.php
@@ -0,0 +1,7 @@
+<?php
+echo '<ul>';
+echo '<li>'."<font color=blue>Disha</font>".'</li>';
+echo '<li>'."<font color=pink>Krishna</font>".'</li>';
+echo '<li>'."<font color=green>Himani</font>".'</li>';
+echo '</ul>';
+?>
