bf53a888d2ed0f948626172502a51f0437a19f92
disha-shiyani <143687230+disha-shiyani@users.noreply.github.com>
2023-10-26 10:24:42 +0530

    Script to find the factorial of a given number and display the result.

---
+++ Task_2/Script_20.php
@@ -0,0 +1,12 @@
+<?php
+$a=10;  
+
+/*Define the variable where you want to store the factorial number value.*/
+$f=1; 
+
+for ($i=1; $i <= $a; $i++) 
+{  
+  $f = $f * $i;  
+} 
+echo "Factorial of $a is $f";  
+?>
