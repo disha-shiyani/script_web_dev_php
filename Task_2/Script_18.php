3c37a7ec918394cab080350b397e1afb676f01a9
disha-shiyani <143687230+disha-shiyani@users.noreply.github.com>
2023-10-26 10:22:22 +0530

    Develop a PHP script that takes a positive integer as input and checks whether it is a prime number.

---
+++ Task_2/Script_18.php
@@ -0,0 +1,10 @@
+<?php 
+$num1 = rand(); 
+  
+echo "Random number: " . $num1 . "\n"; 
+  
+// Generating Random numbers in given range 
+$num2 = rand(1, 10); 
+   
+echo "<br>"."Random number in range (1, 10): ", $num2; 
+ ?>
